a3f599f71ff659bf9a8a8e98957cd9fa72d801a9
JpEscobarM <134341220+JpEscobarM@users.noreply.github.com>
2024-04-24 11:02:14 -0300

    Exercise done

--- Grafos/Beecrowd1148.c
+++ Grafos/Beecrowd1148.c
@@ -2,21 +2,18 @@
 #include <string.h>
 #include <stdlib.h>
 
-
-
-
-
-
-
-
-
 int main(){
 
     int N, E, i, j, orig, dest, h;
 
-    scanf("%d%d",&N&E);
+
+    scanf("%d%d",&N,&E);
+
+    N +=1;
 
     int g[N][N];
+
+
 
     for( i = 0 ;  i < N ; i ++){
         for ( j = 0 ; j < N ; j++){
@@ -27,11 +24,97 @@ int main(){
 
     }
 
+
+
+
+
     for ( i = 0 ; i < E ; i ++){
 
         scanf("%d%d%d", &orig, &dest, &h);
+        g[orig][dest] = h;
+
 
     }
 
 
-return 0;}
+
+    for ( i = 0 ; i< N ; i ++){
+        for ( j = 0 ; j < N ; j ++){
+
+            if ( g [i][j] != -1 && g[j][i] != -1 ) {
+                g[i][j] = g[j][i] = 0;
+            }
+
+
+        }
+    }
+
+
+
+    int K;
+
+
+    scanf("%d", &K);
+
+    int visitados[N], distancia[N], flagDestino;
+
+    for(int s = 0 ; s < K ; s ++){
+
+        scanf("%d%d", &orig,&dest);
+
+        flagDestino = 0 ;
+
+        for ( i  = 0 ; i < N ; i ++){
+            visitados[i] =0; distancia[i] = 99;
+        }
+
+        distancia[orig] = 0 ;
+
+        for ( i  = 0 ; i < N ; i ++){
+
+            int menor = 99, j , cidadeAtual;
+
+            for ( j = 0 ; j < N ; j ++){
+
+                if( visitados[j] == 0 && distancia[j] < menor  ){
+
+                    menor = distancia[j]; cidadeAtual= j ;
+                }
+            }
+
+            if ( cidadeAtual == dest){
+                flagDestino = 1;
+                break;
+            }
+
+        visitados[ cidadeAtual ] = 1;
+
+            for ( j = 0 ; j <N ; j ++){
+
+                if ( g[cidadeAtual][j] != -1 && g[cidadeAtual][j] < distancia[j] ){
+                    distancia[j] = g[cidadeAtual][j] ;
+                }
+
+            }
+
+        }
+        int soma = 0;
+        if( flagDestino == 1){
+        for( i = 0 ; i < N ; i ++){
+            if(distancia[i] != 99){
+            soma += distancia[i];
+            }
+        }
+        printf("%d\n" , soma);
+
+        }
+        else{
+            printf("Nao e possivel entregar a carta\n");
+        }
+    }
+
+
+
+
+return 0;
+}
